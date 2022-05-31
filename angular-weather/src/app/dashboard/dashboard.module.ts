ab5faaec83e35df6e3bfe69524b0afd7a69ba2da
Andrey Rychagov <anryc@yandex.ru>
2022-05-31 18:03:41 +0300

    Add data service

--- angular-weather/src/app/dashboard/dashboard.module.ts
+++ angular-weather/src/app/dashboard/dashboard.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { DataService } from '../services/data.service';
 
 
 @NgModule({
@@ -11,7 +13,11 @@ import { DashboardComponent } from './dashboard.component';
   ],
   imports: [
     CommonModule,
+    HttpClientModule,
     DashboardRoutingModule
+  ],
+  providers: [
+    DataService
   ]
 })
 export class DashboardModule { }
